8cab2be202d73d617183f01f93e90e25c86012c7
claudio nkumbula@gmail.com <klaudio.kumbula@gmail.com>
2021-12-10 14:49:54 +0200

    findUserById

--- src/index.js
+++ src/index.js
@@ -74,11 +74,28 @@ function checksTodoExists(request, response, next) {
 
 }
 
-
-
-
 function findUserById(request, response, next) {
-  // Complete aqui
+
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user){
+    return response.status(404).json({error: 'user not found!!!'});
+  }
+
+  request.user = user;
+
+  return next();
+
+
+
+
+
+
+
+
+
 }
 
 
